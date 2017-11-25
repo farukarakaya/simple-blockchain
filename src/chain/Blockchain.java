851dce23632a874445819b141c408ff249bf2497
farukarakaya <farukarakaya@gmail.com>
2017-11-25 20:06:09 +0300

    Blockhain İmp changed

--- src/chain/Blockchain.java
+++ src/chain/Blockchain.java
@@ -1,5 +1,7 @@
 package chain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 import block.Block;
@@ -7,23 +9,23 @@ import block.BlockUtil;
 
 public class Blockchain {
 
-	private static Stack<Block> blockchain;
+	private static List<Block> blockchain;
 
 	static {
 		initBlockchain();
 	}
 
 	public static Block getLatestBlock() {
-		return blockchain.peek();
+		return blockchain.get(blockchain.size()-1);
 	}
 
 	public static void pushNewBlock(Block new_block) {
-		blockchain.push(new_block);
+		blockchain.add(new_block);
 	}
 
 	private static void initBlockchain() {
-		blockchain = new Stack<Block>();
-		blockchain.push(BlockUtil.generateGenesisBlock());
+		blockchain = new ArrayList<>();
+		blockchain.add(BlockUtil.generateGenesisBlock());
 	}
 
 }
